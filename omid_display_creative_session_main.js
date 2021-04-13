68e1491b646ac1fbde45ef579f67acbbea8876ef
randeepsiddhu <randeepsiddhu@gmail.com>
2021-04-13 16:32:57 -0700

    omid_display_creative_session_main.js

---
+++ omid_display_creative_session_main.js
@@ -0,0 +1,17 @@
+goog.module('omid.creatives.OmidCreativeSessionMain');
+
+const OmidCreativeSession = goog.require('omid.creatives.OmidCreativeSession');
+
+const omidSession = OmidCreativeSession.main([{
+  'resourceUrl': 'https://pubmatic.com',
+    'vendorKey': 'ignored',
+    'verificationParameters': 'ignored'
+}, {
+  'resourceUrl': 'https://s3-us-west-2.amazonaws.com/omsdk-files/js/verification-measurement-script.js',
+  'vendorKey': null,
+  'verificationParameters': null
+}]);
+omidSession.setCreativeType('htmlDisplay');
+omidSession.setImpressionType('loaded');
+omidSession.loaded();
+omidSession.impressionOccurred();
